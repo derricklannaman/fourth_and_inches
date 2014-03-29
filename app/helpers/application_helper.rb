9379789550145814f4fc42c8a8dee77ab905e542
Derrick Lannaman <derricklannaman@gmail.com>
2014-03-29 00:52:22 -0400

    rework of site structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def nav_view_by_login_status
+    if user_signed_in?
+      render 'layouts/user_navigation'
+    else
+      render 'layouts/general_navigation'
+    end
+  end
+
+
 end
