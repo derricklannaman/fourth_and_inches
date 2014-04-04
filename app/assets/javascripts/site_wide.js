37280206e27b73a4f000a86974f4d9bb893b3e1b
Derrick Lannaman <derricklannaman@gmail.com>
2014-04-04 13:56:49 -0400

    fix nab links, remove no ajaxify on links, functioning as expected, next >> merge with development

--- app/assets/javascripts/site_wide.js
+++ app/assets/javascripts/site_wide.js
@@ -1,4 +1,61 @@
 $(function(){
   $('.notice').delay(3000).slideUp(50);
+
+  $('.player-action-sublist, .team-action-sublist').hide();
+  $('#side-panel-list > li:nth-child(5)').hide() // player button
+  $('#player-button').on('click', showPlayerOptions);
+
+
+
+
+  $(document).on('ajaxify:content_loaded', function() {
+    var path = document.location.pathname;
+    console.log(path)
+    $('#bt-menu').removeClass('bt-menu-open').addClass('bt-menu-close');
+
+    if ( path == '/team_manager' ) {
+      setTimeout(function(){
+        $('.team-action-sublist').show();
+        $('#side-panel-list > li:nth-child(5)').show();
+        $('.player-action-sublist').addClass('no-display');
+      }, 10)
+
+    }
+    else if ( path == '/dashboard' ) {
+      hideTeamNavigationOptions();
+    }
+    else if ( path == '/office_manager' ) {
+      hideTeamNavigationOptions();
+    }
+    else if ( path == '/schedule_manager' ) {
+      hideTeamNavigationOptions();
+    }
+    else if ( path == '/coaches_corner' ) {
+      hideTeamNavigationOptions();
+    }
+    else if ( path == '/players' ) {
+      hideTeamNavigationOptions();
+    }
+    else if ( path == '/players/new' ) {
+      hideTeamNavigationOptions();
+    }
+
+
+  });
+
+
 });
+
+function hideTeamNavigationOptions() {
+  $('.player-action-sublist, .team-action-sublist').hide();
+  $('#side-panel-list > li:nth-child(5)').hide();
+}
+
+function showTeamOptions() {
+  $('.team-action-sublist').slideToggle(300)
+}
+
+function showPlayerOptions() {
+  $('.player-action-sublist').removeClass('no-display').slideToggle(150)
+}
 
--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,79 +1,16 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
 $(document).ready(function(){
-  $('.player-action-sublist').hide();
-  $('#player-button').on('click', showPlayerOptions);
-  $('#team-mgr-button').on('click', showTeamOptions);
+
+  $('a[disabled=disabled]').click(function(event){
+      event.preventDefault(); // Prevent link from following its href
+  });
+
+  ///////////// for avgrund modal
   // $('#new-team-side-link').avgrund({
   //     onBlurContainer: '.app-container',
   //     template: $('section#addNewTeam'),
   //     width: 456,
   //     height: 336,
   // });
-
-
-  $('a[disabled=disabled]').click(function(event){
-      event.preventDefault(); // Prevent link from following its href
-  });
-
 });
 
-function showTeamOptions() {
-  $('.team-action-sublist').slideToggle(300)
-}
 
-function showPlayerOptions() {
-  $('.player-action-sublist').slideToggle(150)
-}
-
-// function getTeamInfoToEdit() {
-//   console.log();
-// }
-
-// function createNewTeam() {
-//   var f = $('#new-team-form')[0];
-//   var title = $(f).find('#team_title')[0].value;
-//   var age = $(f).find('#team_age_group')[0].value;
-//   var num = $(f).find('#team_num_of_players')[0].value;
-//   var formData = {
-//     title:  title,
-//     age:    age,
-//     num:    num,
-//   }
-//   $.ajax({
-//     type: 'POST',
-//     url: '/teams',
-//     data: { team: formData },
-//     success: function(result) {
-//       // $(f).hide()
-
-//       var teamShell = result.team_info;
-//       var c = teamShell.name.toUpperCase();
-//       var h = " Football Team";
-//       var g = teamShell.group;
-//       var current = $('section#currentTeam');
-//       current.find('h3').text(c+h);
-//       current.find('h5').text(g);
-//       changeCurrentPlayerCount(current, teamShell);
-//     },
-//     error: function(e) {
-//       alert("Sorry, something when wrong...");
-//     }
-//   });
-// }
-
-// function changeCurrentPlayerCount(current, teamShell) {
-//   var x = current.find('p')[0]
-//   var str = $(x).text()
-//                 .trim()
-//                 .split(' ');
-//   str[str.length-1] = teamShell.num;
-//   var new_num = str.join(' ');
-//   $(x).text(new_num);
-// }
-
-
-// function showNewTeamForm() {
-//   $('#new-team-form').show();
-//   console.log('showNewTeamForm');
-// }
