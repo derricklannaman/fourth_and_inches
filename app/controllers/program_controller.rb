22ab4cdbabe4bc65cfb10c25f501cf33fc4230fd
Derrick Lannaman <derricklannaman@gmail.com>
2014-04-11 13:57:10 -0400

    add routes, form and actions for program

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -3,4 +3,31 @@ class ProgramController < ApplicationController
   def new
     @program = Program.new
   end
+
+  def create
+    @program = Program.new(program_params)
+    @program.user_id = params[:user_id]
+    if @program.save
+      redirect_to @program
+    else
+      flash.notice = @program.errors.full_message.join(' ')
+      render :new
+    end
+  end
+
+  def show
+    @program = Program.find(params[:id])
+    redirect_to(controller: "dashboard", action: "show" )
+  end
+
+
+
+  private
+
+    def program_params
+      params.require(:program).permit(:town_name, :team_name, :league_name)
+    end
+
+
+
 end
