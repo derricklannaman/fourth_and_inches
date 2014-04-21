72d11f49abc83bb0577a858432adc79028f7e042
Derrick Lannaman <derricklannaman@gmail.com>
2014-04-21 15:06:48 -0400

    add msg once program form completes, move to next part on just-in-time form inputs

--- app/assets/javascripts/program.js
+++ app/assets/javascripts/program.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
   $('.not-yet-implemented').on('click', notYetImplemented)
+
+  var theForm = document.getElementById( 'theForm' );
+
+  new stepsForm( theForm, {
+    onSubmit : function( form ) {
+      classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
+      var nextMsg = 'Now, Let\'s add the league divisions';
+      $('#step1').text(nextMsg).hide().fadeIn(500)
+      // var messageEl = theForm.querySelector( '.final-message' );
+      // messageEl.innerHTML = 'Now, Let\'s add the league divisions';
+      // classie.addClass( messageEl, 'show' );
+
+      setTimeout(function(){
+        form.submit()
+      }, 2000)
+
+    }
+  });
+
+
+
+
+
 });
 
 
