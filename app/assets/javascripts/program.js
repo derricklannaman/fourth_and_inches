ada937ab4ae532b372e5bd25ab8621042e0a3f6b
Derrick Lannaman <derricklannaman@gmail.com>
2014-04-22 23:38:56 -0400

    refactor and smoothen flow

--- app/assets/javascripts/program.js
+++ app/assets/javascripts/program.js
@@ -15,78 +15,37 @@ function runStepsForm() {
     onSubmit : function( form ) {
       var fv = $('#form_name')[0].value;
       if (fv === 'add-division') {
-
         alertify.confirm("click OK to Add another division", function(e) {
           if (e) {
-
-            // classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
-            var addAnother = 'Add another division';
+            classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
+            var addAnother = 'Adding another division';
             $('#step2').text(addAnother).hide().fadeIn(500);
             setTimeout(function(){
-              // console.log(form);
               var fv = $('#form_name')[0].value = "add more"
               form.submit()
             }, 600)
-            // console.log('add more');
-
           }
           else {
-
             classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
             var nextMsg = 'Now, Let\'s add the league divisions';
             setTimeout(function(){
               form.submit()
             }, 600)
-
-
-            // console.log('finished');
           }
         });
-
-
-
-
       }
       else if (fv === 'add-program'){
-
-            classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
-            var nextMsg = 'Now, Let\'s add the league divisions';
-            $('div.spinner').show();
-            $('#step1').text(nextMsg).hide().fadeIn(500);
-            setTimeout(function(){
-              form.submit()
-            }, 2000)
-
-
-
+        classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
+        var nextMsg = 'Now, Let\'s add the league divisions';
+        $('div.spinner').show();
+        $('#step1').text(nextMsg).hide().fadeIn(500);
+        setTimeout(function(){
+          form.submit()
+        }, 2000)
       }
-
-
-
-
     }
   });
-
-
-
-
-//////
-  // new stepsForm( theForm, {
-  //   onSubmit : function( form ) {
-  //     classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
-  //     var nextMsg = 'Now, Let\'s add the league divisions';
-  //     $('div.spinner').show();
-  //     $('#step1').text(nextMsg).hide().fadeIn(500);
-  //     setTimeout(function(){
-  //       form.submit()
-  //     }, 2000)
-  //   }
-  // });
-/////
-
 }
-
-
 
 function notYetImplemented() {
   alertify.alert("This feature has not been implemented yet!");
