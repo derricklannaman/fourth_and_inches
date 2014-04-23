d9e171c44587eacf0db04918a654ab15be949be2
Derrick Lannaman <derricklannaman@gmail.com>
2014-04-22 23:02:11 -0400

    add multiple division option

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -11,10 +11,16 @@ class DivisionsController < ApplicationController
 
 
   def create
+    # binding.pry
     @division = Division.new(division_params)
     @division.program_id = params[:program_id]
     if @division.save
-      redirect_to(controller: 'dashboard', action: 'show')
+      # if add another button was clicked, go back to page
+      if params.has_value? "add more"
+        redirect_to :back, notice: 'Please add another division.'
+      else
+        redirect_to(controller: 'dashboard', action: 'show')
+      end
     else
       render 'new'
     end
