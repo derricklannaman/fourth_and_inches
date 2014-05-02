f1b7c29c789c1f0e23f965f2893869081b4b35cc
Derrick Lannaman <derricklannaman@gmail.com>
2014-05-02 14:33:23 -0400

    user crud continues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit( :first_name, :last_name, :username, :email)
+    end
+
 
 
 end
