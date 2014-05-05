06c2810a75fb166c28230b681682a40f4359170d
Derrick Lannaman <derricklannaman@gmail.com>
2014-05-05 15:23:45 -0400

    css - WIP

--- app/assets/javascripts/access_codes.js
+++ app/assets/javascripts/access_codes.js
@@ -1,7 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-
   $('#coach-invite-form, #staff-invite-form').hide();
   $('#coach-invite-button').on('click', toggleCoachAccessForm)
   $('#staff-invite-button').on('click', toggleStaffAccessForm)
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,11 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+
+  $('a.delete-staff').on('click', deleteStaff);
+
+
+})
+
+function deleteStaff() {
+  console.log(this);
+  // alert("This works!");
+}
