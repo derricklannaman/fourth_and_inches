f1b48a9fbb129ee5ce16c49fd731a997a1588ccc
Derrick Lannaman <derricklannaman@gmail.com>
2014-05-08 19:54:31 -0400

    calendar entry WIP

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
-  $('#add-practice, #add-game').on('click', addToCalendar);
   $('div.form-wrapper').hide();
-
+  $('#add-practice, #add-game').on('click', toggleScheduleForm);
+  $('.submit-schedule').on('click', addPracticeToCalendar);
 
 });
 
-function addToCalendar() {
+function addPracticeToCalendar() {
+  var form = $(this).closest('form');
+  var type = form.find('input')[2].value
+  var date = form.find('input')[3].value
+  var time = form.find('input')[4].value
+
+  var practice = {
+    type: type,
+    date: date,
+    time: time
+  }
+  console.log(practice)
+  // ajax call to schedule#create
+}
+
+function toggleScheduleForm() {
   $('div.form-wrapper').toggle(0, function(){
     var cal = $('#schedule-manager');
     cal.toggleClass('schedule-manager-buffer');
