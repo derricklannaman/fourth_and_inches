80b93c8295540e286894d0eb44763e1ebb02b3d2
Derrick Lannaman <derricklannaman@gmail.com>
2014-05-13 22:08:45 -0400

    standardizing forms - WIP

--- app/controllers/opponents_controller.rb
+++ app/controllers/opponents_controller.rb
@@ -1,13 +1,37 @@
 class OpponentsController < ApplicationController
+
+  # before_action :find_opponent, only: [:create, :show]
+
   def new
     @opponent = Opponent.new
     @program_id = current_user.program.id
   end
 
-  def edit
+  def create
+    @opponent = Opponent.new(opponent_params)
+    if @opponent.save
+      redirect_to teams_path, notice: "opponent added"
+    else
+      render 'new'
+    end
+  end
 
+  def edit
+    @opponent = Opponent.find(params[:id])
   end
 
   def show
   end
+
+
+  private
+
+    def opponent_params
+      params.require(:opponent).permit(:name, :location, :program_id,
+                                        :team_id, :division)
+    end
+
+
+
+
 end
