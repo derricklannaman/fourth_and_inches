aeca0e439bd9d69c8a1224af9b675373fd6da819
Derrick Lannaman <derricklannaman@gmail.com>
2014-06-13 10:30:22 -0400

    move sign up elements from website to application helper, change team_path to team_dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,13 @@ module ApplicationHelper
     end
   end
 
+  def show_sign_out_if_signed_in
+    if current_user.nil?
+      return
+    else
+      render 'layouts/user_access_controls'
+    end
+  end
 
   def name
     "#{current_user.first_name} #{current_user.last_name}"
--- app/helpers/websites_helper.rb
+++ app/helpers/websites_helper.rb
@@ -1,10 +1,2 @@
 module WebsitesHelper
-
-  def show_sign_out_if_signed_in
-    if current_user.nil?
-      return
-    else
-      render 'layouts/user_access_controls'
-    end
-  end
 end
