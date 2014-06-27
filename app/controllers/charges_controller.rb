6365d2898c36d509fe7625d92df9dc9a8e82883e
Derrick Lannaman <derricklannaman@gmail.com>
2014-06-27 19:32:14 -0400

    some refactoring of charger controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,14 +8,8 @@ class ChargesController < ApplicationController
   end
 
   def create
-    # old_bal = current_user.account.balance
     old_bal = current_user.account_balance
-    # fee = current_user.formatted_fee * current_user.get_parents_players.count
-
-    # Amount in cents
-
-    # calculate_stripe_fee(fee)
-    calculate_stripe_fee(old_bal)
+    calculate_stripe_fee(old_bal) # conversion in cents for stripe to process
 
     customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
@@ -32,26 +26,54 @@ class ChargesController < ApplicationController
     transaction = Transaction.create( payment: charge[:amount], user_id: current_user.id,
                         account_id: current_user.account.id )
 
-    amt = transaction.payment / 100
-    new_bal = old_bal  - amt
-
-    current_user.account.balance = new_bal
-    current_user.account.save
+    # Update account balances
+    update_account_balances(transaction, old_bal)
 
     # Then update users as registered
-    current_user.hasActiveAccount = true
-    current_user.save
+    set_account_as_active
+    # current_user.hasActiveAccount = true
+    # current_user.save
 
-    players = current_user.get_parents_players
-    players.each do |player|
-     player.is_registered = true
-     player.save
-    end
+    set_player_as_active
+
+    # players = current_user.get_parents_players
+    # players.each do |player|
+    #  player.is_registered = true
+    #  player.save
+    # end
 
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
   end
+
+  private
+
+    def update_account_balances(transaction, old_bal)
+      amt = transaction.payment / 100
+      new_bal = old_bal - amt
+      current_user.account.balance = new_bal
+      current_user.account.save
+    end
+
+    def set_account_as_active
+      current_user.hasActiveAccount = true
+      current_user.save
+    end
+
+    def set_player_as_active
+      players = current_user.get_parents_players
+      players.each do |player|
+       player.is_registered = true
+       player.save
+      end
+    end
+
+
+
+
+
+
 
 end
