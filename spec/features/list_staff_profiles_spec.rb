9e575e258c98744e479f7b976fb493a08be694ef
Derrick Lannaman <derricklannaman@gmail.com>
2014-06-28 23:48:33 -0400

    update app to start adding feature test

--- spec/features/list_staff_profiles_spec.rb
+++ spec/features/list_staff_profiles_spec.rb
@@ -1,11 +1,9 @@
-require "spec_helper"
+# describe "Viewing the profile of team coaches" do
 
-describe "Viewing the profile of team coaches" do
+#   it "shows the profiles" do
+#     visit staff_path
 
-  it "shows the profiles" do
-    visit staff_path
+#     expect(page).to have_text('staff profiles')
+#   end
 
-    expect(page).to have_text('staff profiles')
-  end
-
-end
+# end
