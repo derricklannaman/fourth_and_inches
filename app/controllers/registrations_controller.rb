2364a4b02970aa42fe374f3dd22fd5b3b7d42e90
Derrick Lannaman <derricklannaman@gmail.com>
2014-07-18 15:13:37 -0400

    continued method extraction and clean up in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,23 +3,25 @@ class RegistrationsController < Devise::RegistrationsController
   # skip_before_filter :require_no_authentication
 
   def create
-    if params[:form_type] == 'web_sign_up' && params[:user][:user_type].blank?
+    if form_from_web_registration? && user_type_is_blank?
       process_website_registration
       if @user.save
         sign_in_and_redirect @user
+        # sign_in_and_redirect resource
       else
         render :new
       end
-    elsif params[:user][:user_type].present? && params[:user][:user_type] == 'director'
+    elsif user_type_is_present? && user_type_is_director?
       process_director_registration
         if @user.save
           sign_in_and_redirect resource
         else
           render :new, notice: "Please pick an user type"
         end
-    elsif params[:access].present?
+    elsif access_code_present?
       process_access_code_registration
       if @user.save
+        # set_access_codes
         @code.user_id = @user.id # Set access code to new user if saved
         @code.save
         # TODO: post a confirmation that a coach signed up to directors' activity feed
@@ -39,35 +41,65 @@ class RegistrationsController < Devise::RegistrationsController
       dashboard_path
     end
 
-    def process_website_registration
-      site_id = params.fetch(:site_id)
-      @user = User.new(user_params)
-      @user.user_type = 'parent_user'
-      @user.program_id = params[:program_id]
+    def form_from_web_registration?
+      params[:form_type] == 'web_sign_up'
     end
 
-    def process_director_registration
-      @user = User.new(user_params)
-      user_type = params[:user][:user_type]
-      user_name = params[:user][:first_name] + '_' + params[:user][:last_name]
-      @user.user_type = user_type
-      @user.username = user_name
+    def user_type_is_director?
+      params[:user][:user_type] == 'director'
     end
 
-    def process_access_code_registration
-     @user = User.new(user_params)
-     user_name = params[:user][:first_name] + '_' + params[:user][:last_name]
+    def access_code_present?
+      params[:access].present?
+    end
 
-      parts = params[:access].split('-')
-      code_num = parts[0]                 # Find access code decifer
-      id = parts[2]                       # Find program id
-      @code = AccessCode.where(access_code: code_num)[0]
-      # decifer_access_codes
+    def user_type_is_present?
+      params[:user][:user_type].present?
+    end
+
+    def user_type_is_blank?
+      params[:user][:user_type].blank?
+    end
+
+    def create_new_user
+      @user = User.new(user_params)
+    end
+
+    def decifer_access_codes(parts)
       if parts[1] == "1819055"
         @user.user_type = "staff"
       elsif parts[1] == '7403214027'
         @user.user_type = "head_coach"
       end
+    end
+
+    def process_website_registration
+      create_new_user
+      site_id = params.fetch(:site_id)
+      @user.user_type = 'parent_user'
+      @user.program_id = params[:program_id]
+    end
+
+    def set_user_full_name
+      user_name = params[:user][:first_name] + '_' + params[:user][:last_name]
+    end
+
+    def process_director_registration
+      create_new_user
+      user_type = params[:user][:user_type]
+      set_user_full_name
+      @user.user_type = user_type
+      @user.username = user_name
+    end
+
+    def process_access_code_registration
+      create_new_user
+      set_user_full_name
+      parts = params[:access].split('-')
+      code_num = parts[0]                 # Find access code
+      id = parts[2]                       # Find program id
+      @code = AccessCode.where(access_code: code_num)[0]
+      decifer_access_codes(parts)
       @user.program_id = id
       @user.username = user_name
     end
